3c6e25f8615021dd4a5eb6a972c932f9c12d9f1e
Michael C <michael@mchang.name>
2023-02-28 21:50:44 -0500

    create barebones server

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import Fastify from "fastify"
+import { port } from "./config.json"
+import { routes as wireguard } from "./wireguard"
+
+const fastify = Fastify()
+
+fastify.register(wireguard, { prefix: "/wireguard" });
+
+fastify.get("*", (req, reply) => {
+  reply.status(404).send();
+});
+
+fastify.listen({ port });
+console.log(`server started on port ${port}`);
---
+++ src/wireguard.ts
@@ -0,0 +1,54 @@
+import { exec } from "child_process";
+import { auth, allowed_ifaces } from "./config.json"
+
+export async function routes(fastify, options) {
+  fastify.addHook("preValidation", (req, reply, done) => {
+    if (req.query.auth !== auth) {
+      reply.code(403);
+      done(new Error("Not authorized"));
+    }
+    if (!allowed_ifaces.includes(req.params.iface)) {
+      reply.code(403);
+      done(new Error("Disallowed interface"));
+    } 
+    done();
+  });
+
+  fastify.delete("/:iface", async function (req, reply) {
+    const { pubkey } = req.query
+    const { iface } = req.params
+    const command = `wg set ${iface} peer ${pubkey} remove`
+    exec(command, (error, stdout, stderr) => {
+      if (error || stderr) {
+        return reply.status(500).send({ error: error || stderr })
+      }
+      reply.status(200).send({ message: "Peer removed" })
+    });
+  });
+  fastify.post("/:iface", async function (req, reply) {
+    const { pubkey, endpoint, allowedips } = req.query
+    const { iface } = req.params
+    const command = `wg set ${iface} peer ${pubkey} endpoint ${endpoint} allowed-ips ${allowedips}/32`
+    exec(command, (error, stdout, stderr) => {
+      if (error || stderr) {
+        return reply.status(500).send({ error: error || stderr })
+      }
+      reply.status(201).send({ message: "Peer added" })
+    });
+  });
+  fastify.get("/:iface", async function (req, reply) {
+    const { iface } = req.params
+    const command = `wg show ${iface} allowed-ips`
+    exec(command, (error, stdout, stderr) => {
+      if (error || stderr) {
+        return reply.status(500).send({ error: error || stderr })
+      }
+      if (!stdout.length) return reply.status(204).send({ peers: [] })
+      const peers = stdout.split("\n").map(peer => {
+        const [allowedips, pubkey] = peer.split("\t")
+        return { pubkey, allowedips: allowedips.split(" ") }
+      })
+      reply.status(200).send({ peers })
+    });
+  });
+}
